2e2cac7c9687035b1da1071ef9f92ed90449cd71
Biyannn <ayyubian07@gmail.com>
2026-02-06 21:10:52 +0700

    feat:add service_test.go

--- todo/service_test.go
+++ todo/service_test.go
@@ -1,79 +1,79 @@
 package todo
 
-// import "testing"
+import "testing"
 
-// func resetTodos() {
-// 	todos = []Todo{}
-// 	autoID = 1
-// }
+func resetTodos() {
+	todos = []Todo{}
+	autoID = 1
+}
 
-// func testCreateTodo(t *testing.T) {
-// 	resetTodos()
+func testCreateTodo(t *testing.T) {
+	resetTodos()
 
-// 	todo := Create("Belajar Go", "2026-01-27", "high")
+	todo := Create("Belajar Go", "2026-01-27", "high")
 
-// 	if todo.ID != 1 {
-// 		t.Errorf("Expected ID 1, got %d", todo.ID)
-// 	}
+	if todo.ID != 1 {
+		t.Errorf("Expected ID 1, got %d", todo.ID)
+	}
 
-// 	if todo.Completed != false {
-// 		t.Errorf("Expected Completed false, got %v", todo.Completed)
-// 	}
+	if todo.Completed != false {
+		t.Errorf("Expected Completed false, got %v", todo.Completed)
+	}
 
-// 	if len(todos) != 1 {
-// 		t.Errorf("Expected 1 todo, got %d", len(todos))
-// 	}
-// }
+	if len(todos) != 1 {
+		t.Errorf("Expected 1 todo, got %d", len(todos))
+	}
+}
 
-// func TestGetAllTodos(t *testing.T) {
-// 	resetTodos()
+func TestGetAllTodos(t *testing.T) {
+	resetTodos()
 
-// 	Create("Todo 1", "", "")
-// 	Create("Todo 2", "", "")
+	Create("Todo 1", "", "")
+	Create("Todo 2", "", "")
 
-// 	all := GetAll()
+	all := GetAll()
 
-// 	if len(all) != 2 {
-// 		t.Errorf("expected 2 todos, got %d", len(all))
-// 	}
-// }
+	if len(all) != 2 {
+		t.Errorf("expected 2 todos, got %d", len(all))
+	}
+}
 
-// func TestToggleDone(t *testing.T) {
-// 	resetTodos()
+func TestToggleDone(t *testing.T) {
+	resetTodos()
 
-// 	todo := Create("Belajar Go", "", "")
+	todo := Create("Belajar Go", "", "")
 
-// 	updated, success := ToggleDoneByID(todo.ID)
-// 	if !success {
-// 		t.Fatal("expected toggle success")
-// 	}
+	updated, success := ToggleDoneByID(todo.ID)
+	if !success {
+		t.Fatal("expected toggle success")
+	}
 
-// 	if updated.Completed != true {
-// 		t.Errorf("expected Completed true, got %v", updated.Completed)
-// 	}
-// }
+	if updated.Completed != true {
+		t.Errorf("expected Completed true, got %v", updated.Completed)
+	}
+}
 
-// func TestDeleteTodo(t *testing.T) {
-// 	resetTodos()
+func TestDeleteTodo(t *testing.T) {
+	resetTodos()
 
-// 	todo := Create("Delete me", "", "")
+	todo := Create("Delete me", "", "")
 
-// 	success := DeleteByID(todo.ID)
-// 	if !success {
-// 		t.Fatal("expected delete success")
-// 	}
+	success := DeleteByID(todo.ID)
+	if !success {
+		t.Fatal("expected delete success")
+	}
 
-// 	if len(todos) != 0 {
-// 		t.Errorf("expected 0 todos, got %d", len(todos))
-// 	}
-// }
+	if len(todos) != 0 {
+		t.Errorf("expected 0 todos, got %d", len(todos))
+	}
+}
 
-// func TestDeleteTodoNotFound(t *testing.T)  {
-// 	resetTodos()
+func TestDeleteTodoNotFound(t *testing.T)  {
+	resetTodos()
 
-// 	success := DeleteByID(999)
+	success := DeleteByID(999)
 
-// 	if success {
-// 		t.Error("expected delete to fail, but succeeded")
-// 	}
-// }
+	if success {
+		t.Error("expected delete to fail, but succeeded")
+	}
+}
